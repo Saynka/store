7d55e6d044086570459869b9dfd82dfaef14f2e1
Saynka <sankakale@gmail.com>
2021-07-01 14:42:17 -0700

    future project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import App from "./App";
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* StripeProvider initializes the stripe and passes in the publishable key */}
+
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
+
+    </BrowserRouter>
+  </Provider >
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(app, document.getElementById("root"));
